78fb4573f4915da990cab1571b8ac20e3aa48ca8
Karim Hodzic <Karim.Hodzic@gmail.com>
2026-02-03 22:21:26 -0600

    Added homework page

---
+++ wdv341.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>WDV341 Homework Folder</title>
+    </head>
+    <body class="background">
+        
+        <main class="container">
+
+            <h1>Welcome</h1>
+
+            <p><a href="../index.html">Home</a></p>
+            
+            <nav class="navbar">
+                <p href="">Homework:</p>
+                
+                <ul class="container">
+                    <!--<li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li> -->
+                </ul>
+            
+            </nav>
+
+        </main>        
+    
+    </body>
+</html>
